c0bcaff4a597f4bc17ff255d2039cd19b294d685
Katia Mendes <122563827+katia-mendes@users.noreply.github.com>
2023-06-29 19:49:39 -0300

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 export function login(email,password){
   //chamando a API
-    return fetch('https://burger-queen-api-mock-38om.vercel.app/login', {
+    return fetch('https://burger-queen-api-mock-gules.vercel.app', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
