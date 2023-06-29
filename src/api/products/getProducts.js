8271a29b2216dc769faf3cae0249296bc1f0e0a9
Katia Mendes <122563827+katia-mendes@users.noreply.github.com>
2023-06-29 19:53:33 -0300

    Update getProducts.js

--- src/api/products/getProducts.js
+++ src/api/products/getProducts.js
@@ -3,7 +3,7 @@ import { getItem } from "../../storage/local";
 export function getProducts() {
     const token = getItem('accessToken');
     //chamando a API
-      return fetch('https://burger-queen-api-mock-38om.vercel.app/products', {
+      return fetch('https://burger-queen-api-mock-gules.vercel.app/products', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
